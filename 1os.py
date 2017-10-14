d04285b3ce7f23dd05e4d9e179d2aa0817f4e760
twoyoungtwosimple <32393269+twoyoungtwosimple@users.noreply.github.com>
2017-10-14 10:51:08 +0800

    os
    os

---
+++ 1os.py
@@ -0,0 +1,15 @@
+import os
+print(os.environ)
+# print(os.environ.get('path'))
+# ./1os.py
+# print(os.curdir)
+#
+# path = r'C:\Users\xlg\Desktop\县区列表list.txt'
+# # print(os.path.splitext(path))
+
+# print(os.path.exists(path))
+
+# print(os.path.dirname(path))
+# print(os.path.basename(path))
+# print(os.path.abspath('./1os.py'))
+# print(os.path.split(path))
